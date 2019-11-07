a449989b25abcf6d23c01712604bde663ffb0922
AngelHander <qq844185733@outlook.com>
2019-11-07 09:27:38 +0800

    1

---
+++ OnlineExam/Areas/Student_info/Controllers/InfoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineExam.Areas.Student_info.Controllers
+{
+    public class InfoController : Controller
+    {
+        // GET: Student_info/Info
+        public ActionResult Index(String id)
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView(id);
+            }
+            else
+            {
+                return View(id);
+            }
+        }
+    }
+}
---
+++ OnlineExam/Areas/Student_info/Student_infoAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace OnlineExam.Areas.Student_info
+{
+    public class Student_infoAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Student_info";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Student_info_default",
+                "Student_info/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
