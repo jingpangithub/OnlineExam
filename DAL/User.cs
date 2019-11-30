ac4b77ef1d0e281c44424a3427e8da0d82ed9353
jingpangithub <2038815974@qq.com>
2019-12-01 03:06:52 +0800

    1

---
+++ DAL/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExam.DAL
+{
+    /// <summary>
+	/// User
+	/// </summary>
+    public partial class User
+    {
+
+    }
+}
