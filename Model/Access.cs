8dbf524392f2af0a69dc94bc857cfb026d9a439c
jingpangithub <2038815974@qq.com>
2019-12-01 08:06:13 +0800

    三层架构

---
+++ Model/Access.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OnlineExam.Model
+{
+    /// <summary>
+	/// Access:实体类
+	/// </summary>
+	[Serializable]
+    public partial class Access
+    {
+        public Access()
+        { }
+        #region Model
+        private int id;
+        private string tablename;
+        private string tableadress;
+        
+        public int ID
+        {
+            set { id = value; }
+            get { return id; }
+        }
+        
+        public string TableName
+        {
+            set { tablename = value; }
+            get { return tablename; }
+        }
+        
+        public string TableAdress
+        {
+            set { tableadress = value; }
+            get { return tableadress; }
+        }
+        #endregion Model
+
+    }
+}
--- OnlineExam/Controllers/LoginController.cs
+++ OnlineExam/Controllers/LoginController.cs
@@ -1,19 +1,103 @@
-﻿using OnlineExam.Models;
+﻿using OnlineExam.Codes;
+using OnlineExam.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using Maticsoft.DBUtility;
+using System.Web.Script.Serialization;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace OnlineExam.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
-        public ActionResult Index(LoginModel model)
-        {   
-            
-        }   
+        public ActionResult Index()
+        {
+            Session["status"] = Request.Params["status"];
+
+            return View();
+        }
+
+        //User属于SQL保留字，建表注意表名设计
+        [Login(Feedback = -7)]
+        public ActionResult Login(string Username, string Password)
+        {
+            OnlineExam.BLL.UserTable bllUser = new OnlineExam.BLL.UserTable();
+            if (string.IsNullOrEmpty(Username) || string.IsNullOrEmpty(Password))
+            {
+                return this.Json(new { result = 0, data = "" });
+            }
+            // Password = encryptPwd(Password);
+
+            string strSql = "Username='" + Username + "' and Password='" + Password + "'";
+            List<Model.UserTable> userList = bllUser.DataTableToList(bllUser.GetList(strSql).Tables[0]);
+
+            //authority
+            if (userList.Count > 0)
+            {
+                Model.UserTable userModel = userList[0];
+                strSql = "select * from AccessTable where ID in (select TableID from PermissionTable where UserID = " + userModel.ID + ")";
+                List<Model.Access> tableList = new BLL.Access().DataTableToList(DbHelperSQL.Query(strSql).Tables[0]);
+
+                //get granted table
+                string sessionString = "";
+                if (tableList.Count != 0)
+                {
+                    sessionString += "[";
+                    foreach (Model.Access tableModel in tableList)
+                    {
+                        sessionString += new JavaScriptSerializer().Serialize(tableModel) + ",";
+                    }
+                    sessionString = sessionString.Remove(sessionString.Length - 1);
+                    sessionString += "]";
+                }
+                //set session time out
+                Session.Timeout = 30;
+                Session["username"] = userModel.Username;
+                Session["access"] = sessionString;
+                return this.Json(new { result = 1, data = "" });
+            }
+            else
+            {
+                return this.Json(new { result = 0, data = "" });
+            }
+        }
+
+        /*此方法已过时
+        public string encryptPwd(string pwd)
+        {
+            return System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(pwd, "MD5");
+        }
+        */
+
+        //MD5散列
+        private static string encryptPwd(string pwd)
+        {
+            MD5CryptoServiceProvider md5Hasher = new MD5CryptoServiceProvider();
+            byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(pwd));
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            return sBuilder.ToString();
+        }
+        
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.Cache.SetExpires(DateTime.UtcNow.AddHours(-1));
+            Response.Cache.SetNoStore();
+
+            return RedirectToAction("Index", "Login");
+            //return this.Json(new { result = 1, data = "" });
+        }  
     }
 }
